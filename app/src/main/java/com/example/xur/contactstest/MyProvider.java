b2406ba58b0bd03c746706832e75fafd1d647189
xur <645531705@qq.com>
2017-02-07 13:57:01 +0800

    201702071356

--- app/src/main/java/com/example/xur/contactstest/MyProvider.java
+++ app/src/main/java/com/example/xur/contactstest/MyProvider.java
@@ -2,6 +2,7 @@ package com.example.xur.contactstest;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -10,13 +11,37 @@ import android.net.Uri;
  */
 
 public class MyProvider extends ContentProvider {
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM = 1;
+    public static final int TABLE2_DIR = 2;
+    public static final int TABLE2_ITEM = 3;
+    private static UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.app.provider", "table1", TABLE1_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table1/#", TABLE1_ITEM);
+        uriMatcher.addURI("com.example.app.provider", "table2", TABLE2_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table2/#", TABLE2_ITEM);
+    }
+
     @Override
-    public boolean onCreate(){
+    public boolean onCreate() {
         return false;
     }
+
     @Override
-    public Cursor query(Uri uri,String[] projection,String selection,String[] selectionArgs,String sortOrder){
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                break;
+            case TABLE1_ITEM:
+                break;
+            case TABLE2_DIR:
+                break;
+            case TABLE2_ITEM:
+                break;
+        }return null;
     }
     @Override
     public Uri insert(Uri uri, ContentValues values){
